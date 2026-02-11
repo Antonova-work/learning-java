a9fab7fac1b22efecf4ba36dce18404be448bf76
Anastasiya <aadworkmaill@gmail.com>
2026-02-11 12:58:21 +0300

    add another message in test-massages branch

--- src/lesson1/HelloWorld.java
+++ src/lesson1/HelloWorld.java
@@ -3,5 +3,6 @@ package lesson1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("A little change");
+        System.out.println("Testing branches in Git!");
     }
 }
