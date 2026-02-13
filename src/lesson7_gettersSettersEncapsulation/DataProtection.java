675773ab2fd95a5de90b79335c9f6e7373d01de6
Anastasiya <aadworkmaill@gmail.com>
2026-02-13 19:14:18 +0300

    add exercise on working with getters, setters and encapsulation

---
+++ src/lesson7_gettersSettersEncapsulation/DataProtection.java
@@ -0,0 +1,39 @@
+package lesson7_gettersSettersEncapsulation;
+
+public class DataProtection {
+    public static void main(String[] args) {
+        Restaurant myRestaurant = new Restaurant("Пицца-Парк");
+        System.out.println("Текущее имя ресорана: " + myRestaurant.getName());
+        myRestaurant.setRating(4.5);
+        System.out.println("Текущий рейтинг ресорана: " + myRestaurant.getRating());
+    }
+
+}
+
+class Restaurant {
+    private String name;
+    private double rating;
+
+    public Restaurant (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setRating (double newRating){
+        if (newRating < 0.0) {
+            this.rating = 0.0;
+        }
+        else if (newRating > 5.0){
+            this.rating = 5.0;
+        } else {
+            this.rating = newRating;
+        }
+    }
+
+    public double getRating (){
+        return rating;
+    }
+}
